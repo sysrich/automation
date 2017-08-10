4520d81ad798f74401268136908816c50ecbd649
Kiall Mac Innes <kiall@macinnes.ie>
2017-08-10 10:58:59 +0100

    Generate SSH Config file from env.json
    Additionally, add private/public IPv4 distinction

--- testinfra/tools/generate_ssh_config.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat $1/tools/ssh_config.example | sed "s|%PATH%|$1/tools|" > $1/tools/ssh_config
--- testinfra/tools/testAllRoles.sh
+++ testinfra/tools/testAllRoles.sh
@@ -6,10 +6,8 @@ SSH_CONFIG=$2
 POSARGS=${3:-}
 
 for role in "worker" "master" "admin"; do
-    ips=$(jq -r "[.minions[] | select(.role==\"$role\") | .ipv4 ] | join(\",\" )" $FILE)
-    if [ -n "$ips" ]
-        then
-            pytest --ssh-config=$SSH_CONFIG --sudo -m "$role or common" --hosts $ips --junit-xml $role.xml -v $POSARGS
+    fqdns=$(jq -r "[.minions[] | select(.role==\"$role\") | .fqdn ] | join(\",\" )" $FILE)
+    if [ -n "$fqdns" ]; then
+        pytest --ssh-config=$SSH_CONFIG --sudo -m "$role or common" --hosts $fqdns --junit-xml $role.xml -v $POSARGS
     fi
 done
-
