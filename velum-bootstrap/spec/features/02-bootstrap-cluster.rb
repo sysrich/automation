d37eb1d229e55e23b36bcff7874d0dd70c06ea2d
Kiall Mac Innes <kiall@macinnes.ie>
2017-11-08 12:52:04 +0000

    Split out the kubeconfig download
    This allows this step to be ran separately to to the deployment
    itself.

--- velum-bootstrap/spec/features/02-bootstrap-cluster.rb
+++ velum-bootstrap/spec/features/02-bootstrap-cluster.rb
@@ -133,23 +133,4 @@ feature "Boostrap cluster" do
     end
     puts "<<< Orchestration completed"
   end
-
-  scenario "User downloads the kubeconfig file" do
-    visit "/"
-
-    expect(page).to have_text("You currently have no nodes to be accepted for bootstrapping", wait: 120)
-
-    expect(page).to have_text("kubectl config")
-    with_screenshot(name: :download_kubeconfig) do
-      click_on "kubectl config"
-    end
-    expect(page).to have_text("Log in to Your Account")
-    with_screenshot(name: :oidc_login) do
-      fill_in "login", with: "test@test.com"
-      fill_in "password", with: "password"
-      click_button "Login"
-    end
-    expect(page).to have_text("apiVersion")
-    File.write("kubeconfig", Nokogiri::HTML(page.body).xpath("//pre").text)
-  end
 end
