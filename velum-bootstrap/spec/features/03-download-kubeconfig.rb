d217338ee3af95c3a9b2e7c432486c4273703999
Kiall Mac Innes <kiall@macinnes.ie>
2017-12-01 12:47:20 +0000

    Update kubeconfig download to work on OS and Bare Metal
    In non-caasp-kvm environments, the download flow was different. This was done as
    capybara + poltergeist are unable to download files. Lets download it ourselves,
    without capybara.

--- velum-bootstrap/spec/features/03-download-kubeconfig.rb
+++ velum-bootstrap/spec/features/03-download-kubeconfig.rb
@@ -1,5 +1,7 @@
 require "spec_helper"
+require 'uri'
 require 'yaml'
+require 'net/http'
 
 feature "Download Kubeconfig" do
   before(:each) do
@@ -19,17 +21,44 @@ feature "Download Kubeconfig" do
 
     expect(page).to have_text("You currently have no nodes to be accepted for bootstrapping", wait: 240)
 
+    puts ">>> User clicks to download kubeconfig"
     expect(page).to have_text("kubectl config")
     with_screenshot(name: :download_kubeconfig) do
       click_on "kubectl config"
     end
+    puts "<<< User clicks to download kubeconfig"
+
+    puts ">>> User logs in to Dex"
     expect(page).to have_text("Log in to Your Account")
     with_screenshot(name: :oidc_login) do
       fill_in "login", with: "test@test.com"
       fill_in "password", with: "password"
       click_button "Login"
     end
-    expect(page).to have_text("apiVersion")
-    File.write("kubeconfig", Nokogiri::HTML(page.body).xpath("//pre").text)
+    puts "<<< User logs in to Dex"
+
+    if page.html =~ /apiVersion/
+      # CaaSP KVM Only
+      expect(page).to have_text("apiVersion")
+      File.write("kubeconfig", Nokogiri::HTML(page.body).xpath("//pre").text)
+    else
+      puts ">>> User is redirected back to velum"
+      expect(page).to have_text("You will see a download dialog")
+      puts "<<< User is redirected back to velum"
+
+      # OpenStack/Baremetal/etc
+      puts ">>> User is prompted to download the kubeconfig"
+      download_uri = URI(page.html.match(/window\.location\.href = "(.*?)"/).captures[0])
+
+      http = Net::HTTP.new(download_uri.host, download_uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(download_uri.request_uri)
+      response = http.request(request)
+
+      File.write("kubeconfig", response.body)
+      puts "<<< User is prompted to download the kubeconfig"
+    end
   end
 end
