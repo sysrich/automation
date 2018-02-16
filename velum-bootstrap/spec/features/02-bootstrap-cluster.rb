209c551fd373f9775eae3ab2e1e9a61f256383e6
Kiall Mac Innes <kiall@macinnes.ie>
2018-02-16 17:33:24 +0000

    Put the 30 sec sleep back

--- velum-bootstrap/spec/features/02-bootstrap-cluster.rb
+++ velum-bootstrap/spec/features/02-bootstrap-cluster.rb
@@ -49,6 +49,12 @@ feature "Boostrap cluster" do
       click_button("accept-all")
     end
 
+    puts ">>> Waiting 30 seconds as a workaround"
+    # ugly workaround for https://bugzilla.suse.com/show_bug.cgi?id=1050450
+    # FIXME: drop it when bug is fixed
+    sleep 30
+    puts "<<< Waiting 30 seconds as a workaround"
+
     with_status_ok do
       visit "/setup/discovery"
     end
