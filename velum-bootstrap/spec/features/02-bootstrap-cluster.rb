ead6dc91d533634218f8574dd42c7446ffa6d510
Kiall Mac Innes <kiall@macinnes.ie>
2018-02-16 20:37:31 +0000

    Increase node acceptance delay
    Increase node acceptance delay back to 120 seconds

--- velum-bootstrap/spec/features/02-bootstrap-cluster.rb
+++ velum-bootstrap/spec/features/02-bootstrap-cluster.rb
@@ -49,11 +49,11 @@ feature "Boostrap cluster" do
       click_button("accept-all")
     end
 
-    puts ">>> Waiting 30 seconds as a workaround"
+    puts ">>> Waiting 120 seconds as a workaround"
     # ugly workaround for https://bugzilla.suse.com/show_bug.cgi?id=1050450
     # FIXME: drop it when bug is fixed
-    sleep 30
-    puts "<<< Waiting 30 seconds as a workaround"
+    sleep 120
+    puts "<<< Waiting 120 seconds as a workaround"
 
     with_status_ok do
       visit "/setup/discovery"
